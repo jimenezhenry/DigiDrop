915fb4c84a8b4363c0a977de6709166f24f13cdc
jimenezhenry <jimenez.henry@rocketmail.com>
2017-05-02 13:19:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/henry/digidrop/tab1.java
@@ -0,0 +1,20 @@
+package com.example.henry.digidrop;
+
+/**
+ * Created by Henry on 5/2/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class tab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/henry/digidrop/tab3.java
@@ -0,0 +1,24 @@
+package com.example.henry.digidrop;
+
+/**
+ * Created by Henry on 5/2/2017.
+ */
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Henry on 5/1/2017.
+ */
+
+public class tab3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+        return rootView;
+    }
+}
